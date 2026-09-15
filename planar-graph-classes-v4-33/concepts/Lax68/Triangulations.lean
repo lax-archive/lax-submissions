f179bc81671b353be657432784d7dee68486b0bd
clemenskuske <33251410+clemenskuske@users.noreply.github.com>
2026-09-15 17:32:36 +0200

    Pin corrected triangulation illustration

--- planar-graph-classes-v4-33/concepts/Lax68/Triangulations.lean
+++ planar-graph-classes-v4-33/concepts/Lax68/Triangulations.lean
@@ -6,7 +6,7 @@ title: Triangulations
 type: definition
 ---
 
-![Triangulation illustration](https://raw.githubusercontent.com/lax-archive/lax-submissions/4dd917c8b9a1e181ec951b7f6fa384262af31fa9/planar-graph-classes-v4-33/assets/triangulation.svg "Triangulation illustration")
+![Triangulation illustration](https://raw.githubusercontent.com/lax-archive/lax-submissions/6a23e9f09597af8e83514df2442e5a47ed6ab095/planar-graph-classes-v4-33/assets/triangulation.svg "Triangulation illustration")
 
 A triangulation of a graph G is a planar supergraph T on the same vertex set,
 with at least three vertices, to which no edge can be added while preserving
